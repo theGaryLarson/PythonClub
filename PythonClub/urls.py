3935d05db4def08f34d75492216542d9513968e4
Gary Larson <gary.larson316@gmail.com>
2022-01-07 13:07:17 -0800

    hid secret key with ini file

--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # todo: get this working after migration
+    # path('tech/'), include(Club.urls)
 ]
