d6f87e2a63c3fed21c2ea529cf966e8d3563dc20
Gary Larson <gary.larson316@gmail.com>
2022-01-19 15:15:41 -0800

    Assignment 3: First View

--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Club/'), include('Club.urls'),
+    path('Club/', include('Club.urls')),
 ]
