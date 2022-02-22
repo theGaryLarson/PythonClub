f610fed015318c008a3cf8d0225f8880cbe3db6c
Gary Larson <gary.larson316@gmail.com>
2022-02-21 23:00:10 -0800

    Assignment 7 : Tests

--- club/tests.py
+++ club/tests.py
@@ -1,3 +1,190 @@
+import datetime
+from .views import index, get_tags, get_comments, get_snippets, snippet_detail
+from django.urls import reverse
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+from PythonClub import settings
+from .models import Tag, Snippet, Comment
+
+
 # Create your tests here.
+
+
+class TagTestCase(TestCase):
+    def setup(self):
+        test_tag = Tag()
+        test_tag.keyword = "Beginner"
+        test_tag.tag_description = "Code focused on syntax and entry level programming techniques."
+        return test_tag
+
+    def test_table(self):
+        self.assertEqual(str(Tag._meta.db_table), 'tag')
+
+    def test_table_plural(self):
+        self.assertEqual(str(Tag._meta.verbose_name_plural), 'tags')
+
+    def test_string(self):
+        tag = self.setup()
+        self.assertEqual(str(tag), tag.keyword)
+
+    def test_keyword(self):
+        tag = self.setup()
+        self.assertEqual(tag.keyword, "Beginner")
+
+    def test_description(self):
+        tag = self.setup()
+        self.assertEqual(tag.tag_description, "Code focused on syntax and entry level programming techniques.")
+
+
+class SnippetTestCase(TestCase):
+    def setup(self):
+        test = Snippet()
+        test.snippet_title = "Making Web Request"
+        # todo: look at documentation
+        # test.tag = "Web Scraping"
+        test.user = User()
+        test.user.username = 'garyl'
+        test.snippet_entrydate = datetime.date.today()
+        test.reference_url = "https://www.google.com"
+        test.code_snippet = "print(hello world!)"
+        return test
+
+    def test_table(self):
+        self.assertEqual(str(Snippet._meta.db_table), 'snippet')
+
+    def test_table_plural(self):
+        self.assertEqual(str(Snippet._meta.verbose_name_plural), 'snippets')
+
+    def test_title(self):
+        snippet = self.setup()
+        self.assertEqual(snippet.snippet_title, 'Making Web Request')
+
+    # todo: get tags assigned and tested
+    # def test_tag(self):
+    #     snippet = self.setup()
+    #     self.assertEqual(snippet.tag, 'Beginner')
+
+    def test_user(self):
+        snippet = self.setup()
+        self.assertEqual(snippet.user.username, 'garyl')
+
+    def test_entrydate(self):
+        snippet = self.setup()
+        self.assertEqual(snippet.snippet_entrydate, datetime.date.today())
+
+    def test_url(self):
+        snippet = self.setup()
+        self.assertEqual(snippet.reference_url, 'https://www.google.com')
+
+    def test_code_snippet(self):
+        snippet = self.setup()
+        self.assertEqual(snippet.code_snippet, 'print(hello world!)')
+
+
+class CommentTestCase(TestCase):
+    def setup(self):
+        test = Comment()
+        test.comment_title = "Making Web Request"
+        test.comment_date = datetime.date.today()
+        test.snippet = Snippet()
+        test.snippet.snippet_title = 'snippet title'
+        # todo: look up documentation to test many to many relationships
+        # test.user = User()
+        # test.user.username = 'garyl'
+        test.snippet_rating = 5
+        test.discussion_text = 'Let us talk about this ...'
+        return test
+
+    def test_table(self):
+        self.assertEqual(str(Comment._meta.db_table), 'comment')
+
+    def test_table_plural(self):
+        self.assertEqual(str(Comment._meta.verbose_name_plural), 'comments')
+
+    def test_title(self):
+        comment = self.setup()
+        self.assertEqual(comment.comment_title, 'Making Web Request')
+
+    def test_date(self):
+        comment = self.setup()
+        self.assertEqual(comment.comment_date, datetime.date.today())
+
+    def test_snippet(self):
+        comment = self.setup()
+        self.assertEqual(comment.snippet.snippet_title, 'snippet title')
+
+    # todo: get this working
+    # def test_user(self):
+    #     comment = self.setup()
+    #     self.assertEqual(comment.user.username, 'garyl')
+
+    def test_rating(self):
+        comment = self.setup()
+        self.assertEqual(comment.snippet_rating, 5)
+
+    def test_discussion_test(self):
+        comment = self.setup()
+        self.assertEqual(comment.discussion_text, 'Let us talk about this ...')
+
+
+class IndexTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class GetSnippetsTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('snippets'))
+        self.assertEqual(response.status_code, 200)
+
+
+class GetCommentsTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('comments'))
+        self.assertEqual(response.status_code, 200)
+
+
+class GetTagsTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('tags'))
+        self.assertEqual(response.status_code, 200)
+
+
+class GetSnippetDetail(TestCase):
+    # todo: many to many setup for details isn't working getting same error as above
+    # todo: Snippet.tag(many to many) Comment.user(many to many)
+    def setup(self):
+        self.user = User.objects.create(username='myuser')
+        self.tag = Tag.objects.create(keyword='Beginner')
+        self.snip = Snippet.objects.create(snippet_title='snippetTitle',
+                                           tag=self.tag,
+                                           user=self.user,
+                                           snippet_entrydate=datetime.date.today(),
+                                           reference_url='https://www.google.com',
+                                           code_snippet='codeSnippet')
+
+        self.comment1 = Comment.objects.create(comment_title='snippetreview',
+                                               comment_date=datetime.date.today(),
+                                               snippet=self.snip,
+                                               discussion_test='discussion about snippet')
+
+        self.comment2 = Comment.objects.create(comment_title='snippetreview',
+                                               comment_date=datetime.date.today(),
+                                               snippet=self.snip,
+                                               discussion_test='discussion about snippet')
+        self.comment1.user.add(self.user)
+        self.comment2.user.add(self.user)
+
+    # todo: research why many to many is not working
+    # def test_number_of_reviews(self):
+    #     self.setup()
+    #     comments = Comment.objects.filter(product=self.snip).count()
+    #     self.assertEqual(comments, 2)
+
+    # def test_snippet_detail_success(self):
+    #     self.setup()
+    #     response = self.client.get(reverse('snippet_detail', args=(self.snip.id,)))
+    #     # Assert that self.post is actually returned by the post_detail view
+    #     self.assertEqual(response.status_code, 200)
